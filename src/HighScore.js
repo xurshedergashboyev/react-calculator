3fb56acabe381a520e92f82e49451dbb8863e9c1
xurshedergashboyev <62480704+xurshedergashboyev@users.noreply.github.com>
2020-09-13 01:32:47 -0700

    Add files via upload

---
+++ src/HighScore.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+class HighScore extends Component {
+    
+    render() { 
+        if( this.props.overTen) {
+        return (  
+            <h3>Beat High score of 10!
+            <button onClick={this.props.onReset}>Reset</button>
+        </h3>)
+        ;
+   } else {
+       return null;
+   }
+ } 
+}
+ 
+export default HighScore;
